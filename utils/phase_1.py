8231304371be8e0cff679ad9b1ad7c7e5d2b9460
jakub-ner <kubaner1@gmail.com>
2024-10-23 10:23:55 +0200

    change paths

--- utils/phase_1.py
+++ utils/phase_1.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-def take_photo():
+def take_photo() -> cv2.typing.MatLike: # type: ignore
   camera = cv2.VideoCapture(0)
   if not camera.isOpened():
     raise Exception("Could not open video device")
@@ -14,3 +14,11 @@ def take_photo():
     # cv2.imwrite("debug-photo.jpg", frame)
     camera.release()
     return frame
+  
+def downscale_img(img, target_size: tuple[int, int]): ...
+
+def pick_biggest(face_boxes: list[np.ndarray]): ...
+
+def crop_face_from_img(img: np.ndarray, face_box: np.ndarray) -> np.ndarray:
+  x, y, w, h = face_box
+  return img[y:y+h, x:x+w]
