85c8333cf8eead867c06b79c7bf79f9a2768a088
jakub-ner <kubaner1@gmail.com>
2024-10-23 12:20:33 +0200

    phase 1 jupyter and utils

--- utils/phase_1.py
+++ utils/phase_1.py
@@ -1,7 +1,13 @@
 import cv2
 import numpy as np
+from cv2.typing import MatLike
+from matplotlib.patches import Circle, Rectangle
+import matplotlib.pyplot as plt
+import torch
 
-def take_photo() -> cv2.typing.MatLike: # type: ignore
+from utils.libs.BlazeFace.blazeface import BlazeFace
+
+def take_photo() -> MatLike: # type: ignore
   camera = cv2.VideoCapture(0)
   if not camera.isOpened():
     raise Exception("Could not open video device")
@@ -15,10 +21,72 @@ def take_photo() -> cv2.typing.MatLike: # type: ignore
     camera.release()
     return frame
   
-def downscale_img(img, target_size: tuple[int, int]): ...
+def reverse_channels(img: MatLike) -> MatLike:
+  return img[:, :, ::-1] # equivalent to cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-def pick_biggest(face_boxes: list[np.ndarray]): ...
+def downsize_img(img: MatLike, target_size: tuple[int, int]):
+  return cv2.resize(img, target_size)
 
-def crop_face_from_img(img: np.ndarray, face_box: np.ndarray) -> np.ndarray:
-  x, y, w, h = face_box
-  return img[y:y+h, x:x+w]
+def get_torch_device():
+  print("PyTorch version:", torch.__version__)
+  print("CUDA version:", torch.version.cuda)
+  print("cuDNN version:", torch.backends.cudnn.version())
+  device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+  return device
+
+def load_blazeface(path:str, device: torch.device) -> BlazeFace:
+  model = BlazeFace().to(device)
+  model.load_weights(path + "blazeface.pth")
+  model.load_anchors(path + "anchors.npy")
+  return model
+
+def configure_params(model, min_score_thresh, min_suppression_threshold):
+  model.min_score_thresh = min_score_thresh
+  model.min_suppression_threshold = min_suppression_threshold
+
+def plot_detections(img, detections, with_keypoints=True):
+    _, ax = plt.subplots(1, figsize=(5, 5))
+    ax.grid(False)
+    ax.imshow(img)
+    
+    if isinstance(detections, torch.Tensor):
+        detections = detections.cpu().numpy()
+
+    if detections.ndim == 1:
+        detections = np.expand_dims(detections, axis=0)
+
+    print("Found %d faces" % detections.shape[0])
+        
+    for i in range(detections.shape[0]):
+        y_min = detections[i, 0] * img.shape[0]
+        x_min = detections[i, 1] * img.shape[1]
+        y_max = detections[i, 2] * img.shape[0]
+        x_max = detections[i, 3] * img.shape[1]
+
+        rect = Rectangle((x_min, y_min), x_max - x_min, y_max - y_min,
+                                 linewidth=1, edgecolor="g", facecolor="none", 
+                                 alpha=detections[i, 16])
+        ax.add_patch(rect)
+
+        CIRCLE_SETTINGS = {"radius": 0.5, "linewidth": 2, "edgecolor": "r", "facecolor": "none"}
+        if with_keypoints:
+            for k in range(6):
+                kp_x = detections[i, 4 + k*2    ] * img.shape[1]
+                kp_y = detections[i, 4 + k*2 + 1] * img.shape[0]
+                circle = Circle((kp_x, kp_y), **CIRCLE_SETTINGS, alpha=detections[i, 16])
+                ax.add_patch(circle)
+    plt.show()
+
+
+def get_idx_of_biggest_face(detections: np.ndarray) -> int: 
+   return int(np.apply_along_axis(lambda x: (x[2] - x[0]) * (x[3] - x[1]), 1, detections).argmax())
+
+
+def crop_face_from_img(img: MatLike, detections: MatLike) -> MatLike:
+  y_min = int(detections[0] * img.shape[0])
+  x_min = int(detections[1] * img.shape[1])
+  y_max = int(detections[2] * img.shape[0])
+  x_max = int(detections[3] * img.shape[1])
+
+  print(y_min, x_min, y_max, x_max)
+  return img[y_min:y_max, x_min:x_max]
